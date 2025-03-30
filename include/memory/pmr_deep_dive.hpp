44194b81e339fa1d7e62387d374b39c6e8196cc2
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-03-30 15:57:26 +0800

    wip: another deep dive in memory

--- include/memory/pmr_deep_dive.hpp
+++ include/memory/pmr_deep_dive.hpp
@@ -4,6 +4,7 @@
 #include <cctype>
 #include <fmt/core.h>
 #include <fmt/ranges.h>
+#include <memory>
 
 template <typename ItrBegin, typename ItrEnd>
 void print_line(const int offset, ItrBegin begin, const ItrEnd end) {
@@ -41,22 +42,80 @@ void print_buffer(const std::string_view title, const Buffer &buffer,
   fmt::print("\n");
 }
 
-template <typename Buffer, typename Container>
-void print_buffer20(const std::string_view title, const Buffer &buffer,
-                  const Container &container) {
-  fmt::print("======={:^25}=======\n", title);
 
-  auto begin = buffer.begin();
-  fmt::print("Buffer Address Start: {}\n",
-             static_cast<const void *>(buffer.data()));
+// Define a custom allocator
+template <typename T>
+struct MyAllocator {
+  using value_type = T;
 
-  for (const auto &e : container) {
-    fmt::print(" Item Address: {}\n", static_cast<const void *>(&e));
+  // Allocate memory
+  T* allocate(size_t n) {
+    return static_cast<T*>(std::malloc(n * sizeof(T)));
   }
 
-  for (auto offset = 0uz; offset < buffer.size(); offset+=20) {
-    print_line(offset, std::next(begin, offset), std::next(begin, offset + 20));
+  // Deallocate memory
+  // size_t is to conform with standards of allocator requirements
+  // where std::vector, std::list, etc. does
+  void deallocate(T* p, [[maybe_unused]] size_t n) {
+    std::free(p);
   }
 
-  fmt::print("\n");
-}
+  // Construct an object in memory
+  void construct(T* p, const T& value) {
+    new (p) T(value);
+  }
+
+  // Destroy an object in memory
+  void destroy(T* p) {
+    p->~T();
+  }
+};
+
+// Allocator-aware class
+template <typename T, typename Allocator = std::allocator<T>>
+class AllocatorAwareClass {
+public:
+  using value_type = T;
+  using allocator_type = Allocator;
+
+  // Default constructor
+  AllocatorAwareClass() : data_(nullptr), size_(0), allocator_() {}
+
+  // Constructor that takes an allocator
+  explicit AllocatorAwareClass(const allocator_type& allocator) : data_(nullptr), size_(0), allocator_(allocator) {}
+
+  // Constructor that takes an allocator and an initial size
+  AllocatorAwareClass(const allocator_type& allocator, size_t size) : data_(nullptr), size_(0), allocator_(allocator) {
+    if (size > 0) {
+      data_ = allocator_.allocate(size);
+      size_ = size;
+    }
+  }
+
+  // Destructor
+  ~AllocatorAwareClass() {
+    if (data_) {
+      for (size_t i = 0; i < size_; ++i) {
+        allocator_.destroy(&data_[i]);
+      }
+      allocator_.deallocate(data_, size_);
+      data_ = nullptr;
+      size_ = 0;
+    }
+  }
+
+  // Get the allocator
+  allocator_type get_allocator() const {
+    return allocator_;
+  }
+
+  // Access data
+  T& operator[](size_t index) {
+    return data_[index];
+  }
+
+private:
+  T* data_;
+  size_t size_;
+  allocator_type allocator_;
+};
--- src/memory/pmr_deep_dive.cpp
+++ src/memory/pmr_deep_dive.cpp
@@ -6,6 +6,7 @@
 #include <memory_resource>
 #include <string>
 #include <vector>
+#include <iostream>
 
 void freed_resources() {
   // 16 byte for allocation
@@ -89,6 +90,170 @@ void nested_string_pmr() {
   print_buffer("2short and 1long pmr strings", buffer, vec1);
 }
 
+/**
+ * \brief example for 
+ */
+struct S
+{
+  /// Member variable
+  std::pmr::string str = "default_value";
+
+  /// This is required for the class to be allocator aware
+  using allocator_type = std::pmr::polymorphic_allocator<>;
+
+  /// Default constructor, delegate to allocator aware constructor
+  S() = default;
+  // S() : S(allocator_type{}) {}
+
+  explicit S(allocator_type alloc) : str("allocator_constructor", alloc) {}
+
+  /// Copy constructor
+  S(const S &other, allocator_type alloc = {})
+    : str(other.str, alloc)
+    {
+      ;
+    }
+
+  /// Move constructor
+  S(S &&) = default;
+
+  /// Move constructor, with allocator
+  S(S &&other, allocator_type alloc)
+    : str(std::move(other.str), alloc)
+  {}
+
+  /// Copy assignment
+  S &operator=(const S &rhs) = default;
+
+  /// Move assignment
+  S &operator=(S &&rhs) = default;
+
+  ~S() = default;
+
+  allocator_type get_allocator() const {
+    return str.get_allocator();
+  }
+};
+/**
+ * \brief Allocator aware class!
+ *        If you would like to use pmr data in your class, these are the
+ *        things that you would have to pay attention to.
+ *        Notice that there may be memory fragmentation happening ref to below
+ *        vid: https://www.youtube.com/watch?v=2LAsqp7UrNs&t=873s
+ *
+ *        Or look at the output of the code, after updating vec1[0].str, the
+ *        address is pointed to somewhere else in the memory down below.
+ */
+void allocator_aware_class() {
+  fmt::print("Size of `S()`: {}\n", sizeof(S));
+
+  std::array<std::uint8_t, 304> buffer{};
+  std::pmr::monotonic_buffer_resource memory_resource(buffer.data(), buffer.size());
+
+  fmt::print("memory resource address: {}\n", static_cast<const void *>(&memory_resource));
+  print_buffer("initial", buffer, "");
+
+  std::pmr::vector<S> vec1(&memory_resource);
+  vec1.push_back(S());
+  print_buffer("1 S Object", buffer, vec1);
+
+  vec1.emplace_back();
+  print_buffer("2 S Object", buffer, vec1);
+
+  // A different long string
+  vec1[0].str = "A different long string";
+  print_buffer("Updated string", buffer, vec1);
+}
+
+/**
+ * \brief A helpful wrapper for debugging where pmr allocator is really
+ *        being used.
+ */
+class print_resource : public std::pmr::memory_resource {
+private:
+  void* do_allocate(std::size_t bytes, std::size_t alignment) override {
+    std::cout << "Allocating " << bytes << '\n';
+    return std::pmr::new_delete_resource()->allocate(bytes, alignment);
+  }
+
+  void do_deallocate(void *p, std::size_t bytes, std::size_t alignment) override {
+
+    std::cout << "Deallocating " << bytes << ": '";
+    for (auto i = 0uz; i < bytes; ++i) {
+      std::cout << *(static_cast<char*>(p) + i);
+    }
+    std::cout << "'\n";
+
+    return std::pmr::new_delete_resource()->deallocate(p, bytes, alignment);
+  }
+
+  bool do_is_equal(const std::pmr::memory_resource &other) const noexcept override {
+    return std::pmr::new_delete_resource()->is_equal(other);
+  }
+};
+
+void initializer_list_and_pmr() {
+  // Initializer list are broken (memory allocation happens and they are being
+  // copied over when std::initializer_list<> is used)
+  // vid: https://www.youtube.com/watch?v=sSlmmZMFsXQ
+
+  // Set our memory resource wrapper as default
+  print_resource default_memory_resource;
+  std::pmr::set_default_resource(&default_memory_resource);
+
+  std::array<std::uint8_t, 8> small_buffer{};
+  std::pmr::monotonic_buffer_resource small_memory_resource{small_buffer.data(), small_buffer.size()};
+
+  std::cout << "Small buffer pmr vector\n";
+  std::pmr::vector<std::pmr::string> vec{{"Hello", "World"}, &small_memory_resource};
+
+  std::array<std::uint8_t, 1024> big_buffer{};
+  std::pmr::monotonic_buffer_resource big_memory_resource{big_buffer.data(), big_buffer.size()};
+
+  std::cout << "Scope Starts!\n\n";
+  {
+    // Notice "Hello Long String" is allocated with our wrapper memory resource
+    std::pmr::vector<std::pmr::string> vec2{{"Hello", "World", "Hello Long String"}, &big_memory_resource};
+    std::cout << "vec2 created!!\n";
+
+    // Notice "Another long string created!" is also allocated
+    // with default memory resource, because for vector push back, it needs to
+    // first create a pmr::string, which is on the using our wrapper memory resource
+    // then it copies over, it cannot be moved since it is from a different memory resource!
+    vec2.push_back("Another long string created!");
+
+    // The one way for us is to use emplace_back!
+    // Notice wrapper memory resource is not used
+    vec2.emplace_back("weehee but please used reserve first!");
+  }
+  std::cout << "Scope Ends!\n\n";
+
+  std::cout << "Exit function!\n";
+}
+
+// A better way is to write a helper function that emplace_back all values
+template <typename Container, typename... Values>
+auto create_contianer(auto *resource, Values&&... values) {
+  Container result{resource};
+  result.reserve(sizeof...(values));
+  (result.emplace_back(std::forward<Values>(values)), ...);
+  return result;
+}
+void no_initializer_list_pmr() {
+  // Set our memory resource wrapper as default
+  print_resource default_memory_resource;
+  std::pmr::set_default_resource(&default_memory_resource);
+
+  std::array<std::uint8_t, 1024> buffer{};
+  std::pmr::monotonic_buffer_resource memory_resource{buffer.data(), buffer.size()};
+
+  std::cout << "Start of function!\n";
+  auto vec = create_contianer<std::pmr::vector<std::pmr::string>>(&memory_resource,
+      "Hello", "World", "Hello Long String", "Another long string created!",
+      "weehee but please used reserve first!");
+  std::cout << "Exit of function!\n";
+}
+
 int main ([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 {
   fmt::print("\n\n\n======={:^25}=======\n\n\n", "Freed Resources");
@@ -102,6 +267,15 @@ int main ([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 
   fmt::print("\n\n\n======={:^25}=======\n\n\n", "Short String PMR");
   nested_string_pmr();
+
+  fmt::print("\n\n\n======={:^25}=======\n\n\n", "Allocator Aware Class");
+  allocator_aware_class();
+
+  fmt::print("\n\n\n======={:^25}=======\n\n\n", "Initializer List & PMR");
+  initializer_list_and_pmr();
+
+  fmt::print("\n\n\n======={:^25}=======\n\n\n", "No Initializer List & PMR");
+  no_initializer_list_pmr();
 
   return EXIT_SUCCESS;
 }
